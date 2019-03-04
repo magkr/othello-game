220a310184cf0855bdfe0ca54d4d2027c1e0cf0b
magkr <magkr@itu.dk>
2019-03-04 14:26:08 +0100

    det virker næsten

--- GameState.java
+++ GameState.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 /**
  * Class to represent the state of a game of Othello.  The state is defined by a 2-dimensional
- * board and whose turn it is. 
+ * board and whose turn it is.
  * @author Mai Ajspur
  * @version 9.2.2018
  */
@@ -10,17 +10,17 @@ public class GameState {
 	private int[][] board; 		// Possible values: 0 (empty), 1 (black), 2 (white)
 	private int currentPlayer; 	// The player who is next to put a token on the board. Value is 1 or 2.
 	private int size;  			// The number of columns = the number of rows on the board
-	
+
 	//************ Constructors ****************//
 	/**
-	 * Initializes a square board with the number of columns and rows equal to the given size. 
-	 * The two middle positions on the left-leaning diagonal contains tokens for black (player 1), 
+	 * Initializes a square board with the number of columns and rows equal to the given size.
+	 * The two middle positions on the left-leaning diagonal contains tokens for black (player 1),
 	 * the two middle positions on the right-leaning diagonal contains token for white (player 2).
 	 * @param size Number of columns (and number of rows) in the board. Should be an even number
 	 * greater or equal to 4.
 	 * @param playerToStart The player who will go first. Should be 1 (black) or 2 (white).
 	 */
-	public GameState(int size, int playerToStart){ 
+	public GameState(int size, int playerToStart){
 		this.size = size;
 		board = new int[size][size];
 		currentPlayer = playerToStart;
@@ -28,17 +28,17 @@ public class GameState {
         board[half][half] = 1;
         board[half+1][half+1] = 1;
         board[half][half+1] = 2;
-        board[half+1][half] = 2;   
+        board[half+1][half] = 2;
 	}
-	
+
 	/**
 	 * Constructs a new game state that equals the one represented by the supplied board and player.
 	 * @param board The 2 dimensions of the array should have equal length, and possible values should be
 	 * 0 (empty), 1 (black) or 2 (white).
-	 * @param playerToTakeTurn The player who will be the first to take a turn. Should be 1 (black) 
+	 * @param playerToTakeTurn The player who will be the first to take a turn. Should be 1 (black)
 	 * or 2 (white)
 	 */
-	public GameState(int[][] board, int playerToTakeTurn){ 
+	public GameState(int[][] board, int playerToTakeTurn){
 		this.size = board.length;
 		this.board = new int[size][size];
 		for (int i = 0; i < size; i++){
@@ -48,7 +48,7 @@ public class GameState {
 		}
 		this.currentPlayer = playerToTakeTurn;
 	}
-	
+
 	//************ Getter methods *******************//
 	/**
 	 * Returns the array representing the board of this game state
@@ -71,7 +71,7 @@ public class GameState {
 	public void changePlayer(){
 		currentPlayer = currentPlayer == 1 ? 2 : 1;
 	}
-	
+
 	/**
 	 * Returns true if the game is finished (i.e. none of the players can make any legal moves)
 	 * and false otherwise.
@@ -89,7 +89,7 @@ public class GameState {
 			}
 		}
 	}
-	
+
 	/**
 	 * Counts tokens of the player 1 (black) and player 2 (white), respectively, and returns an array
 	 * with the numbers in that order.
@@ -107,13 +107,13 @@ public class GameState {
     	}
     	return new int[]{tokens1, tokens2};
 	}
-	
+
 	/**
-	 * If it is legal for the current player to put a token at the given place, then the token is inserted, the required 
-	 * tokens from the opponent is turned, and true is returned. If the move is not legal, false is returned. 
-	 * False is also returned if the given place does not represent a place on the board. 
+	 * If it is legal for the current player to put a token at the given place, then the token is inserted, the required
+	 * tokens from the opponent is turned, and true is returned. If the move is not legal, false is returned.
+	 * False is also returned if the given place does not represent a place on the board.
 	 */
-    public boolean insertToken(Position place) { 
+    public boolean insertToken(Position place) {
     	if ( place.col < 0 || place.row < 0 || place.col >= size || place.row >= size ) //not a position on the board
     		return false;
     	if ( board[place.col][place.row] != 0 ) // The position is not empty
@@ -123,15 +123,15 @@ public class GameState {
     	// Capturing all possible opponents of the current player
     	for (int deltaX = -1; deltaX <= 1; deltaX++){
     		for (int deltaY = -1; deltaY <= 1; deltaY++){
-        		int captives = captureInDirection(place, deltaX, deltaY); 
+        		int captives = captureInDirection(place, deltaX, deltaY);
         		if ( captives > 0){
         			capturesFound = true;
         			for ( int i = 1; i <= captives; i++)
         				board[place.col+deltaX*i][place.row+deltaY*i]=currentPlayer;
         		}
-        	}		
+        	}
     	}
-    	
+
     	if ( capturesFound ){
     		// Place the token at the given place
     		board[place.col][place.row] = currentPlayer;
@@ -166,8 +166,8 @@ public class GameState {
     		}
     	}
     	return legalPlaces;
-    }	
-    
+    }
+
     /**
      * Checks how many tokens of the opponent the player can capture in the direction given by deltaX and deltaY
      * if the player puts a token at the given position.
@@ -176,18 +176,18 @@ public class GameState {
      * @param deltaY The step to be taken in the delta direction. Should be -1 (up), 0 (none), or 1 (down).
      */
     private int captureInDirection(Position p, int deltaX, int deltaY){
-    	int opponent = (currentPlayer == 1 ? 2 : 1); 
-        
+    	int opponent = (currentPlayer == 1 ? 2 : 1);
+
     	int captured = 0;
     	int cc = p.col;
     	int rr = p.row;
-        while ( 0 <= cc+deltaX && cc+deltaX < size && 0 <= rr+deltaY && rr+deltaY < size 
-    			&& board[cc+deltaX][rr+deltaY] == opponent ){ 
+        while ( 0 <= cc+deltaX && cc+deltaX < size && 0 <= rr+deltaY && rr+deltaY < size
+    			&& board[cc+deltaX][rr+deltaY] == opponent ){
         	cc = cc + deltaX;
         	rr = rr + deltaY;
         	captured++;
         }
-        if ( 0 <= cc+deltaX && cc+deltaX < size  && 0 <= rr+deltaY && rr+deltaY < size 
+        if ( 0 <= cc+deltaX && cc+deltaX < size  && 0 <= rr+deltaY && rr+deltaY < size
     			&& board[cc+deltaX][rr+deltaY] == currentPlayer && captured > 0 ){
         	return captured;
         }
--- MyAI.java
+++ MyAI.java
@@ -11,11 +11,74 @@ public class MyAI implements IOthelloAI{
 	 *
 	 */
 	public Position decideMove(GameState s){
-		ArrayList<Position> moves = s.legalMoves();
-		if ( !moves.isEmpty() )
-			return moves.get((int)(Math.random() * moves.size()));
+		if (!s.legalMoves().isEmpty())
+			return miniMax(s);
 		else
 			return new Position(-1,-1);
+	}
+
+	private Position miniMax(GameState s) {
+		//System.out.println("max: " + s.getPlayerInTurn() + " player turn");
+		ArrayList<Position> moves = s.legalMoves();
+		int v = Integer.MIN_VALUE;
+		Position res = new Position(-1,-1);
+		for(Position p : moves) {
+			//System.out.println(p);
+			GameState state = new GameState(s.getBoard(), s.getPlayerInTurn());
+				int m = minValue(result(state, p));
+				//System.out.println(m);
+				if(m > v) {
+					res = p;
+					v = m;
+				}
+		}
+		return res;
+	}
+
+	private int minValue(GameState s) {
+		//System.out.println("min: " + s.getPlayerInTurn() + " player turn");
+		if(s.isFinished()) return utility(s);
+		ArrayList<Position> moves = s.legalMoves();
+		if(moves.isEmpty()) {
+			s.changePlayer();
+			return maxValue(s);
+		}
+		int v = Integer.MAX_VALUE;
+		for(Position p : moves) {
+			v = Math.min(v, maxValue(result(s, p)));
+		}
+		return v;
+	}
+
+	private int maxValue(GameState s) {
+		//System.out.println("max: " + s.getPlayerInTurn() + " player turn");
+		if(s.isFinished()) return utility(s);
+		ArrayList<Position> moves = s.legalMoves();
+		if(moves.isEmpty()) {
+			s.changePlayer();
+			return minValue(s);
+		}
+		int v = Integer.MIN_VALUE;
+		for(Position p : moves) {
+			v = Math.max(v, minValue(result(s, p)));
+		}
+		return v;
+	}
+
+	private GameState result(GameState s, Position a) {
+		s.insertToken(a);
+		return s;
+	}
+
+	private int utility(GameState s) {
+		int[] tokens = s.countTokens();
+		int p = s.getPlayerInTurn();
+		int pt1 =  (p == 1 ? tokens[0] : tokens[1]);
+		int pt2 =  (p == 2 ? tokens[1] : tokens[0]);
+
+		if(pt1 > pt2) return 1;
+		else if (pt1 == pt2) return 0;
+		else return -1;
 	}
 
 }
